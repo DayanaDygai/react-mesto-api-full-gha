7d6f417e7812475bba9a1846cae294ab26576b04
DayanaDygai <126877205+DayanaDygai@users.noreply.github.com>
2024-01-28 23:47:52 +0400

    m

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <h2 className="footer__title">© 2020 Mesto Russia</h2>
-    </footer>
-  );
-}
-
-export default Footer;
--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRouteElement = ({ loggedIn, children }) => {
-  return loggedIn ? children : <Navigate to="/sign-in" replace />;
-};
-
-export default ProtectedRouteElement;
