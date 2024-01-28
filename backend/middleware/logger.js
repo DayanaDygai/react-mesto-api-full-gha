f3f298cc7c74b15d8333c327734aa131b6c8d01e
Dayana Dygai <dayana.dygai@gmail.com>
2024-01-29 02:46:48 +0400

    15

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -3,21 +3,21 @@ import winston from 'winston';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import expressWinston from 'express-winston';
 
-const requestLogger = expressWinston.logger({
+export const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
   ],
   format: winston.format.json(),
 });
 
-const errorLogger = expressWinston.errorLogger({
+export const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.File({ filename: 'error.log' }),
   ],
   format: winston.format.json(),
 });
 
-export default {
-  requestLogger,
-  errorLogger,
-};
+// export {
+//   requestLogger,
+//   errorLogger,
+// };
